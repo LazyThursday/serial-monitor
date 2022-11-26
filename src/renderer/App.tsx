bc71e9689138910a77cfc21b9190d2f79c550a36
LazyThursday <68090691+LazyThursday@users.noreply.github.com>
2022-11-26 18:21:19 +0300

    working chart from serial input

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,106 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 import './App.css';
 
-const Hello = () => {
-  useEffect(() => {
-    function printSerial(_event: any, data: string) {
-      console.log(data);
-    }
+type MyData = { time: number; value: number };
+type UserPref = { min: number | null; max: number | null; range: number };
 
-    window.electron.ipcRenderer.on('serialport', (data) => {
-      console.log(data);
+const Hello = () => {
+  const [data, setData] = useState<MyData[]>([]);
+  const [pref, setPref] = useState<UserPref>({
+    min: null,
+    max: null,
+    range: 100,
+  });
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.electron.ipcRenderer.on('serialport', (datum: string) => {
+      setData((prev) => {
+        const newData = parseFloat(datum);
+        const newTime = Date.now();
+        const newDataPoint = { time: newTime, value: newData };
+        return [...prev, newDataPoint];
+      });
     });
   }, []);
 
+  function updatePref(newValue: number, prefToChange: string) {
+    setPref((prev) => {
+      return { ...prev, [prefToChange]: newValue };
+    });
+  }
+
+  const chartData = useMemo(() => {
+    const newArr = data.map((datum) => {
+      const newTime = (Date.now() - datum.time) / 1000;
+
+      return { time: newTime.toFixed(1), value: datum.value };
+    });
+    if (newArr.length > (pref.range ?? 100)) {
+      return newArr.slice(
+        newArr.length - (pref.range ?? 100),
+        newArr.length - 1
+      );
+    }
+
+    return newArr;
+  }, [data]);
+
+  const domain = useMemo(() => {
+    const onlyData = chartData.map((datum) => datum.value);
+    const min = pref.min ? pref.min : Math.min(...onlyData);
+    const max =
+      pref.max && !(pref.max <= min) ? pref.max : Math.max(...onlyData);
+    return { min, max };
+  }, [chartData]);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="chart-container">
+      <label>
+        min:
+        <input
+          placeholder="min"
+          onChange={(e) =>
+            updatePref(parseFloat(e.target.value), e.target.placeholder)
+          }
+        />
+      </label>
+      <label>
+        max:
+        <input
+          placeholder="max"
+          onChange={(e) =>
+            updatePref(parseFloat(e.target.value), e.target.placeholder)
+          }
+        />
+      </label>
+      <label>
+        range:
+        <input
+          placeholder="range"
+          value={isNaN(pref.range) ? '' : pref.range}
+          onChange={(e) =>
+            updatePref(parseFloat(e.target.value), e.target.placeholder)
+          }
+        />
+      </label>
+      <LineChart width={1000} height={300} data={chartData}>
+        <Line type="monotone" dataKey="value" stroke="#c2d5ff" />
+        {/* <CartesianGrid stroke="#ccc" /> */}
+        <XAxis dataKey="time" />
+        <YAxis domain={[domain.min, domain.max]} />
+      </LineChart>
     </div>
   );
 };
