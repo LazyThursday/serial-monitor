f395daa0c3527d718860cba1158caf2e51042666
LazyThursday <68090691+LazyThursday@users.noreply.github.com>
2022-12-01 22:55:04 +0300

    attempt 3 to fix actions

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
+jest.mock('@electron/remote', () => ({ exec: jest.fn() }));
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
